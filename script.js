574beb09cc7804e1ded87a5923c4da5e4513116a
Alle95 <147329984+Alle95@users.noreply.github.com>
2023-11-23 13:15:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,81 @@
-let arrayOfWords = ["INNORAT", "APARTAMENT", "MASINA","FLOARE", 
-"FURCULITA","ACUARELE"];
-let givenWord = arrayOfWords[Math.floor(Math.random() * arrayOfWords.length)];
-let newWord = [];
-let incorrectGuesses = 0;
-let noOfLetters = 0;
-
-function showWord() {
-    document.getElementById('button-container').innerHTML += `
-    <div class="card">
-        <div class="body-header">Cuvantul este:</div>
-        <br>
-        <input type="text" value="" name="textbox" id="textbox" readonly/>
-        <br>
-        <h4 id="incorrectGuesses" > Raspuns Gresit: 0</h4>
-        <button id="reloadPage" type="button" class="btn btn-secondary" style="float:right" 
-        onClick="window.location.reload()">
-        Reincearca!
-        </button>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Spanzuratoarea</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body" style="background-color: skyblue;">
+                <h1 class="card-title" style="color:black; font-style: oblique;">Spanzuratoarea</h1>
+                <h5 class="cad-title">Regulile jocului! </h5>
+                <p class ="rules">Scopul jocului este de a ghici cuvântul dat,
+                     prin încercări succesive de litere. Cuvântul ce trebuie ghicit
+                     este reprezentat de un șir de linii, fiecare linie reprezentând
+                     o literă a cuvântului. Fiecare încercare are 
+                     ca efect indicarea locurilor în care litera selectată se 
+                     află în interiorul cuvântului, sau, în caz de eşec, se pierde
+                    cate o viata din cele 7. 
+                    Jocul se incheie cand jucatorul ghiceste tot cuvantul, astfel
+                    castigand jocul, sau cand s-au pierdut cele 7 vieti astfel jucatorul
+                    pierzand jocul. Mult succes! 
+                </p>
+                <button id="startGame" type="button" class="btn btn-primary" style="float:right" onclick="showWord()">
+                    Start Joc!
+                </button>
+              </div>
+    </div>
+    <div class="container">
+        <div class="card" style>
+          <div class="body-header">Cuvantul este:</div>
+            <br>
+            <input type="text" value="" name="textbox" id="textbox" readonly/>
+            <br>
+            <h4 id="incorrectGuesses" > Raspuns Gresit: 0</h4>
+          </div>
+          <button id="reloadPage" type="button" class="btn btn-secondary" style="float:left"
+            onClick="window.location.reload()">
+            Reincearca!
+          </button>
         </div>
+    </div>
     <br>
-  `; 
-    for(let i = 65; i <= 90; ++i) {
-        const newBtn = document.createElement('button');
-        newBtn.innerText = String.fromCharCode(i);
-        newBtn.className="btn btn-primary";
-        newBtn.style="margin-right: 16px";
-        document.querySelector('#button-container').appendChild(newBtn);
-        newBtn.id = i;
-        newBtn.addEventListener('click', onClick);
-    }
-    for(let i = 0; i < givenWord.length; ++i) {
-        newWord[i] = "_";
-    }
-    document.getElementById("textbox").value = newWord;
-    document.getElementById("startGame").disabled = true;
-}
-
-const onClick = function() {
-    let letter = String.fromCharCode(this.id); 
-    if (givenWord.includes(letter)) {
-        apperLetter(letter);
-    } else {
-        ++incorrectGuesses;
-        document.getElementById("incorrectGuesses").innerHTML = "Raspuns Gresit:" + incorrectGuesses;
-        if (incorrectGuesses == 7) {
-            document.getElementById("incorrectGuesses").innerHTML = "Ne pare rau ai pierdut!";
-            let elements = document.getElementsByClassName("btn btn-primary");
-            for(var i = 0; i < elements.length; ++i) {
-                elements[i].disabled = true;
-            }
-        }
-    }
-}
-
-function apperLetter(letter) {
-    for(let i = 0; i < givenWord.length; ++i) {
-        if (givenWord[i] == letter) {
-            newWord[i] = letter;
-            ++noOfLetters;
-        }
-    }
-    document.getElementById("textbox").value = newWord;
-    if(noOfLetters == givenWord.length) {
-        document.getElementById("incorrectGuesses").innerHTML = "Felicitari, ai ghicit cuvantul!";
-        let elements = document.getElementsByClassName("btn btn-primary");
-        for(var i = 0; i < elements.length; ++i) {
-            elements[i].disabled = true;
-         }
-    }
-}
+    <br>
+    <br>
+    <div class="container">
+      <div class="keyboard">
+        <button class="btn btn-primary btn-lg" onclick="addLetter('A')">A</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('B')">B</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('C')">C</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('D')">D</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('E')">E</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('F')">F</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('G')">G</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('H')">H</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('I')">I</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('J')">J</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('K')">K</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('L')">L</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('M')">M</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('N')">N</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('O')">O</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('P')">P</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('Q')">Q</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('R')">R</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('S')">S</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('T')">T</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('U')">U</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('V')">V</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('W')">W</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('X')">X</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('Y')">Y</button>
+        <button class="btn btn-primary btn-lg" onclick="addLetter('Z')">Z</button>
+    </div>
+  </div>
+    <script src="script.js"></script>
+  </body>
+</html>
